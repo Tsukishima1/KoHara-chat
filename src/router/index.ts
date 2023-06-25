2c623ac838866f500cf002c9c42acf554e665b3c
Tsukishima1 <1732124764@qq.com>
2023-06-25 17:26:39 +0800

    feat(router): 未登录不可访问主页

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,12 +14,27 @@ const routes: RouteRecordRaw[] = [
 		path: "/chat",
 		name: "chatpage",
 		component: async () => await import("../views/MainView.vue"),
+		// 必须登录才能访问
+		meta: {
+			requiresAuth: true,
+		},
 	},
 ];
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
+});
+
+router.beforeEach((to, from, next) => {
+	const isAuthenticated = sessionStorage.getItem("username");
+	if (to.matched.some((record) => record.meta.requiresAuth)) {
+		if (!isAuthenticated) {
+			next("/");
+		} else {
+			next();
+		}
+	} else next();
 });
 
 export default router;
